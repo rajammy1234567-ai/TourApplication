82fcaf0081f05feeba400119e46890c2956ed9b1
Divyanshu-chauhan000 <divyanshuchauhan15072003@gmail.com>
2026-05-08 16:59:18 +0530

    Updated project

--- ExploreApp-frontend/app/(auth)/signUp.tsx
+++ ExploreApp-frontend/app/(auth)/signUp.tsx
@@ -53,7 +53,7 @@ const register = async () => {
 
     console.log("SENDING:", body); 
 
-    const res = await fetch('https://application-tours.onrender.com/api/auth/register', {
+    const res = await fetch('https://tourapplication.onrender.com/api/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(body),
